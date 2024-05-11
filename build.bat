7c54c81c2997f14844af85a399e99830e7ae2808
Osiris Pedroso <opedroso@gmail.com>
2024-05-11 02:58:39 -0500

    must build with build.bat

--- build.bat
+++ build.bat
@@ -1,10 +1,13 @@
+::
+:: Some tests will fail unless the stack for the executable has been extended beyond 2 MB (In Windows, default it 1MB)
+::
 setlocal
 :: uncomment next line if you want to build using nightly toolchain
 ::set TOOLCHAIN=+nightly
 cargo %TOOLCHAIN% clean
 cargo %TOOLCHAIN% build
 cargo %TOOLCHAIN% test --no-run
-:: this next line sets the stack size for all executables built to be 10 MB
+:: this next line sets the stack size for all executables built to be 10 MB reserved 7 committed
 for /r %%i in (rust*.exe) do "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.39.33519\bin\Hostx64\x64\editbin.exe" /nologo /stack:10485760,10485760 %%i
 cargo %TOOLCHAIN% run --bin rust-define-global-allocator
 cargo %TOOLCHAIN% run --bin rust-paragraph-allocator
