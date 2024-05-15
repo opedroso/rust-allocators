072ee35b158af0607f3038e93dbf90c4c37e6ed8
Osiris Pedroso <opedroso@gmail.com>
2024-05-15 09:51:28 -0500

    works both windows and linux

--- build.bat
+++ build.bat
@@ -14,9 +14,8 @@ cargo %TOOLCHAIN% build %TARGET% %OPTIONS%
 cargo %TOOLCHAIN% test %TARGET%  %OPTIONS% --no-run
 :: this next line sets the stack size for all executables built to be 100 MB reserved & 1MB committed
 for /r %%i in (rust*.exe) do "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.39.33519\bin\Hostx64\x64\editbin.exe" /nologo /stack:104857600,1048576 %%i
-if /I "%~1" == "-d" shift & SET RUST_LOG=debug
+if /I "%~1" == "-d" shift & SET RUST_LOG=info
 cargo %TOOLCHAIN% test %TARGET% %OPTIONS% -- --test-threads=1 --nocapture
 cargo %TOOLCHAIN% run  %TARGET% %OPTIONS% --bin rust-define-global-allocator -- --nocapture
 cargo %TOOLCHAIN% run  %TARGET% %OPTIONS% --bin rust-paragraph-allocator -- --nocapture
-
 endlocal
